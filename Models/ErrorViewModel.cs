f5a85bb0e040ae468e195bf15cf0dcbd70871dab
david.lake89 <david@testifysec.com>
2022-09-14 00:17:19 -0500

    init mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BHM_Meetup_Sept_2022_Polyglot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
